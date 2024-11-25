d02acdd04584559ba7ace8a4631c0b2b1fb292d7
Leah Genkinger <genkinger@protonmail.com>
2024-11-25 20:01:10 +0100

    bump

--- .scripts/kensington.sh
+++ .scripts/kensington.sh
@@ -1,2 +1,3 @@
 # xinput set-button-map $(xinput | rg Kensington | rg -v Keyboard | rg id=\(\\d+\) -or '$1') 1 2 3 4 5 6 7 8 9
-xinput set-button-map $(xinput | rg Kensington | rg -v Keyboard | rg id=\(\\d+\) -or '$1') 1 2 8 4 5 6 7 3 9
+# xinput set-button-map $(xinput | rg Kensington | rg -v Keyboard | rg id=\(\\d+\) -or '$1') 1 2 8 4 5 6 7 3 9
+xinput set-button-map $(xinput | rg Kensington | rg -v Keyboard | rg id=\(\\d+\) -or '$1') 1 2 2 4 5 6 7 3 9
